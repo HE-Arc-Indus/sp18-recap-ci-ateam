ba036df1cb1e37c304d3e24ba9ea25ff5fe642b5
Joyce-he-arc <joyce.dubois@he-arc.ch>
2018-05-13 21:46:15 +0200

    Implémentation des classes FizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,3 +1,35 @@
-public class FizzBuzz {
+import java.util.ArrayList;
+import java.util.List;
 
+public class FizzBuzz {
+  public FizzBuzz() {
+  }
+
+  /**
+   * Applies the fizzbuzz rules to all numbers from one up to length.
+   *
+   * @return complete FizzBuzz solution up to {@code length}
+   */
+  public List<String> generateList(int length) {
+    List<String> fbList = new ArrayList<>();
+    for (int n = 1; n < length + 1; n++) {
+      fbList.add(transformSingleNumber(n));
+    }
+    return fbList;
+  }
+
+  private String transformSingleNumber(int n) {
+    String s = String.valueOf(n);
+    if (n % 3 == 0 && n % 5 == 0) {
+      return "FizzBuzz";
+    }
+    if (n % 3 == 0) {
+      return "Fizz";
+    }
+    if (n % 5 == 0) {
+      return "Buzz";
+    } else {
+      return s;
+    }
+  }
 }
